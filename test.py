be7c1a8e6b93c19845c4d3f0921c352108d1c136
Chao Jiao <32031469+Soulempty@users.noreply.github.com>
2021-10-09 16:51:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,49 @@
-import os
+import os 
 import cv2
 import torch
+import shutil
 import numpy as np
+from time import time
+import torch.nn.functional as F
 from kernal import togray,laplacian,gridImg,gaussion,conv2d
 
-#torch laplacian
-def torch_lap_img(img):
-    input = togray(img)
-    data = torch.from_numpy(input).unsqueeze(0).unsqueeze(0)
+def lap_det(img,r=11,c=7):
+    h,w = img.shape[:2]
+    st = time()
+    tensor = None
+    if torch.cuda.is_available():
+        tensor = torch.from_numpy(img).cuda().float()  
+    else:
+        tensor = torch.from_numpy(img).float()
+    mh,mw = (h-8)%r, (w-8)%c
+    hl,hr = mh//2,mh-mh//2
+    wt,wb = mw//2,mw-mw//2
+    crop = tensor[hl+4:h-hr-4,wt+4:w-wb-4]
+    input = togray(crop)
+    data = gridImg(input,r,c)
+    gass_w = gaussion(3,5)
     weight = laplacian(0)
-    out = torch.abs(conv2d(data,weight))
-    r = out.squeeze(0).squeeze(0).numpy()
-    return r
-
-#opencv laplacian
-def LaplacianTransform(img):
-    dst_img = cv2.Laplacian(img, cv2.CV_32F)
-    laplacian_edge = cv2.convertScaleAbs(dst_img)
-    return laplacian_edge
+    if torch.cuda.is_available():
+        gass_w = gass_w.cuda()
+        weight = weight.cuda()
+    out = torch.abs(conv2d(conv2d(data,gass_w),weight))
+    var = out.reshape(out.size(0),-1).var(1)
+    print("time: ",time()-st)
+    return var
     
-#manual laplacian
-def LaplacianOperator(img,op_type=0):
-    ops = [[[0,1,0],[1,-4,1],[0,1,0]],[[1,1,1],[1,-8,1],[1,1,1]]]
-    operator = np.array(ops[op_type])
-    h,w = img.shape
-    result = np.zeros((h,w))
-    for r in range(1,h-1):
-        for c in range(1,w-1):
-            result[r,c] = abs(np.sum(img[r-1:r+2,c-1:c+2] * operator))
-    return np.uint8(result)
-    
-def get_lap_img(img_path):       
-    img = cv2.imread(img_path,0)
-    print("img:",img)
-    torch_lap = lap_img(img)
-    cv_lap = LaplacianTransform(img)
-    manual_lap = LaplacianOperator(img)
-    
-    print("torch_lap:",torch_lap)
-    print("cv_lap:",cv_lap)
-    print("manual_lap:",manual_lap)
-    
+def get_blur(img_path,save_path="blur"):
+    fs = os.listdir(img_path)
+    for f in fs:
+        path = os.path.join(img_path,f)
+        img = cv2.imread(path)
+        var = torch.sort(lap_det(img))[0]
+        mean_max = ((var-var.mean())**2)[-10:].mean().cpu().numpy()
+        value = var[:50].mean().cpu().numpy()
+        print(mean_max,value)
+        if value<1.0 and mean_max<0.116:
+            shutil.move(path,os.path.join(save_path,f))
+            
 if __name__ == "__main__":
-    img_path = "image/101.jpg"
-    get_lap_img(img_path)
+    img_path = "images"
+    save_path = "blur"
+    get_blur(img_path,save_path)
